565d2f2485bd4a62954f53cbdfcdd50005ba5000
hieundph34772 <150779331+hieundph34772@users.noreply.github.com>
2024-10-16 00:41:09 +0700

    update header

--- client/src/components/layouts/website/Header.tsx
+++ client/src/components/layouts/website/Header.tsx
@@ -6,6 +6,7 @@ const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userName, setUserName] = useState("");
   const [roleId, setRoleId] = useState(null); // Trạng thái để lưu trữ role_id
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Trạng thái cho dropdown
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
 
@@ -17,7 +18,7 @@ const Header = () => {
       setUserName(nameFromStorage || "");
       setRoleId(roleIdFromStorage); // Thiết lập trạng thái role_id
     }
-  }, []);
+  }, [token]);
 
   const handleLogout = () => {
     if (window.confirm("Bạn có chắc chắn muốn đăng xuất không?")) {
@@ -27,7 +28,7 @@ const Header = () => {
       setIsLoggedIn(false);
       setUserName("");
       setRoleId(null);
-      navigate("/");
+      navigate("/login");
     }
   };
 
@@ -35,15 +36,22 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // Hàm xử lý khi nhấp vào tên người dùng
-  const handleUserNameClick = () => {
-    if (roleId === '2') {
-      navigate('/admin'); // Chuyển hướng đến trang quản trị
-    }
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  // Hàm điều hướng đến trang admin
+  const handleGoToAdmin = () => {
+    navigate("/admin");
+  };
+
+  // Hàm điều hướng đến thông tin tài khoản
+  const handleGoToAccountInfo = () => {
+    navigate("/account-info"); // Đường dẫn đến trang thông tin tài khoản
   };
 
   return (
-    <header className="bg-white/100 mb-20 fixed top-0 left-0 right-0 z-50 shadow-lg ">
+    <header className="bg-white/100 mb-20 fixed top-0 left-0 right-0 z-50 shadow-lg">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
@@ -82,40 +90,22 @@ const Header = () => {
 
         {/* Menu cho màn hình lớn */}
         <div className="hidden lg:flex lg:gap-x-12">
-          <Link
-            to="/"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
+          <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">
             Trang chủ
           </Link>
-          <Link
-            to="/about"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
+          <Link to="/about" className="text-sm font-semibold leading-6 text-gray-900">
             Giới Thiệu
           </Link>
-          <Link
-            to="/danhsach"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
+          <Link to="/danhsach" className="text-sm font-semibold leading-6 text-gray-900">
             Danh sách phòng
           </Link>
-          <Link
-            to="/lienhe"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
+          <Link to="/lienhe" className="text-sm font-semibold leading-6 text-gray-900">
             Liên hệ
           </Link>
-          <Link
-            to="/blog"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
+          <Link to="/blog" className="text-sm font-semibold leading-6 text-gray-900">
             Blog
           </Link>
-          <Link
-            to="/history1"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
+          <Link to="/history1" className="text-sm font-semibold leading-6 text-gray-900">
             Lịch sử mua hàng
           </Link>
         </div>
@@ -123,8 +113,11 @@ const Header = () => {
         {/* Hiển thị điều kiện cho đăng nhập/đăng ký hoặc tên người dùng/đăng xuất */}
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           {isLoggedIn ? (
-            <div className="flex space-x-2 items-center">
-              <button className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-1 px-3 rounded-full flex items-center space-x-2 shadow-md text-xs sm:text-sm lg:text-base">
+            <div className="relative">
+              <button
+                className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-1 px-3 rounded-full flex items-center space-x-2 shadow-md text-xs sm:text-sm lg:text-base"
+                onClick={toggleDropdown}
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -139,28 +132,43 @@ const Header = () => {
                     d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zM5.832 17.999C5.44 17.7 5 17.192 5 16.5c0-.828.448-1.5 1.003-1.888C7.146 13.855 9.432 13 12 13c2.568 0 4.854.855 5.997 1.612.555.388 1.003 1.06 1.003 1.888 0 .692-.439 1.2-.832 1.499H5.832z"
                   />
                 </svg>
-                <span onClick={handleUserNameClick} className="cursor-pointer">{userName}</span>
+                <span>{userName}</span>
               </button>
 
-              <button
-                onClick={handleLogout}
-                className="bg-red-600 hover:bg-red-700 text-white font-medium py-1 px-3 rounded-full shadow-md text-xs sm:text-sm lg:text-base"
-              >
-                Đăng xuất
-              </button>
+              {/* Dropdown Menu */}
+              {isDropdownOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg">
+                  {/* Thông tin tài khoản */}
+                  <button
+                    onClick={handleGoToAccountInfo}
+                    className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Thông tin tài khoản
+                  </button>
+                  {/* Trang quản trị */}
+                  {roleId === "2" && (
+                    <button
+                      onClick={handleGoToAdmin}
+                      className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      Trang quản trị
+                    </button>
+                  )}
+                  <button
+                    onClick={handleLogout}
+                    className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Đăng xuất
+                  </button>
+                </div>
+              )}
             </div>
           ) : (
             <>
-              <Link
-                to="/register"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
+              <Link to="/register" className="text-sm font-semibold leading-6 text-gray-900">
                 Đăng ký
               </Link>
-              <Link
-                to="/login"
-                className="ml-6 text-sm font-semibold leading-6 text-gray-900"
-              >
+              <Link to="/login" className="ml-6 text-sm font-semibold leading-6 text-gray-900">
                 Đăng nhập
               </Link>
             </>
@@ -170,54 +178,40 @@ const Header = () => {
         {/* Menu cho màn hình nhỏ */}
         {isMenuOpen && (
           <div className="lg:hidden absolute top-full left-0 right-0 bg-gray-200 shadow-lg p-6 z-40">
-            <Link
-              to="/"
-              className="block text-sm font-semibold leading-6 text-gray-900 mb-4"
-            >
+            <Link to="/" className="block text-sm font-semibold leading-6 text-gray-900 mb-4">
               Trang chủ
             </Link>
-            <Link
-              to="/about"
-              className="block text-sm font-semibold leading-6 text-gray-900 mb-4"
-            >
+            <Link to="/about" className="block text-sm font-semibold leading-6 text-gray-900 mb-4">
               Giới Thiệu
             </Link>
-            <Link
-              to="/danhsach"
-              className="block text-sm font-semibold leading-6 text-gray-900 mb-4"
-            >
+            <Link to="/danhsach" className="block text-sm font-semibold leading-6 text-gray-900 mb-4">
               Danh sách phòng
             </Link>
-            <Link
-              to="/lienhe"
-              className="block text-sm font-semibold leading-6 text-gray-900 mb-4"
-            >
+            <Link to="/lienhe" className="block text-sm font-semibold leading-6 text-gray-900 mb-4">
               Liên hệ
             </Link>
-            <Link
-              to="/blog"
-              className="block text-sm font-semibold leading-6 text-gray-900 mb-4"
-            >
+            <Link to="/blog" className="block text-sm font-semibold leading-6 text-gray-900 mb-4">
               Blog
             </Link>
-            <Link
-              to="/history1"
-              className="block text-sm font-semibold leading-6 text-gray-900 mb-4"
-            >
+            <Link to="/history1" className="block text-sm font-semibold leading-6 text-gray-900 mb-4">
               Lịch sử mua hàng
             </Link>
-            <Link
-              to="/register"
-              className="block text-sm font-semibold leading-6 text-gray-900 mb-4"
-            >
-              Đăng ký
-            </Link>
-            <Link
-              to="/login"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Đăng nhập
-            </Link>
+            {isLoggedIn && (
+              <div>
+                <button
+                  onClick={handleGoToAccountInfo}
+                  className="block w-full text-left text-sm font-semibold leading-6 text-gray-900 mb-4"
+                >
+                  Thông tin tài khoản
+                </button>
+                <button
+                  onClick={handleLogout}
+                  className="block w-full text-left text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Đăng xuất
+                </button>
+              </div>
+            )}
           </div>
         )}
       </nav>
