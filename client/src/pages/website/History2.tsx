e6fd454c48a4ab5378c275613fc35f2858c2eb70
toanminh <toanminh1310@gmail.com>
2024-10-29 22:33:33 +0700

    done pay

--- client/src/pages/website/History2.tsx
+++ client/src/pages/website/History2.tsx
@@ -38,6 +38,11 @@ const History2 = () => {
     const paymentData = data.payment;
     const roomData = paymentData.room;
     const servicesData = paymentData.service;
+    const bookingData = paymentData.booking;
+    const payMethodData = paymentData.paymentMethod;
+    const sizeData = paymentData.size;
+
+    console.log(servicesData);
 
 
     return (
@@ -53,27 +58,27 @@ const History2 = () => {
 
                                     <label className="block">
                                         <span className="text-gray-700 text-lg">Tên khách hàng</span>
-                                        <input type='text' value={paymentData?.payment.user_name} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
+                                        <input readOnly type='text' value={paymentData?.payment.user_name} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
                                     </label>
 
                                     <label className="block">
                                         <span className="text-gray-700 text-lg">Email</span>
-                                        <input type='text' value={paymentData?.payment.user_email} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
+                                        <input readOnly type='text' value={paymentData?.payment.user_email} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
                                     </label>
 
                                     <label className="block">
                                         <span className="text-gray-700 text-lg">Số điện thoại</span>
-                                        <input type='text' value={paymentData?.payment.user_phone} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
+                                        <input readOnly type='text' value={paymentData?.payment.user_phone} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
                                     </label>
 
                                     <label className="block">
                                         <span className="text-gray-700 text-lg">Địa chỉ</span>
-                                        <input type='text' value={paymentData?.payment.user_address} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
+                                        <input readOnly type='text' value={paymentData?.payment.user_address} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
                                     </label>
 
                                     <label className="block">
                                         <span className="text-gray-700 text-lg">Tên thú cưng</span>
-                                        <input type='text' value={paymentData?.payment.pet_name} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
+                                        <input readOnly type='text' value={paymentData?.payment.pet_name} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
                                     </label>
 
                                     <label className="block">
@@ -85,16 +90,14 @@ const History2 = () => {
 
                                     <label className="block">
                                         <span className="text-gray-700 text-lg">Mô tả chi tiết thú cưng (Màu, giống,...)</span>
-                                        <textarea className="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" rows='3'>
+                                        <textarea readOnly className="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" rows='3'>
                                             {paymentData.payment.pet_description}
                                         </textarea>
                                     </label>
 
                                     <label className="block">
                                         <span className="text-gray-700 text-lg">Tình trạng sức khỏe</span>
-                                        <select className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2 mb-5">
-                                            <option value={paymentData?.payment.pet_health}>{paymentData.payment.pet_health}</option>
-                                        </select>
+                                        <input readOnly type='text' value={paymentData?.payment.pet_health} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
                                     </label>
                                 </div>
                             </div>
@@ -110,9 +113,7 @@ const History2 = () => {
                             <div className="max-w-md">
                                 <div className="grid grid-cols-1 gap-6">
                                     <label className="block">
-                                        <select className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2">
-                                            <option value={paymentData?.payment.name}>{paymentData.payment.name}</option>
-                                        </select>
+                                        <input readOnly type='text' value={payMethodData} className="block w-full mt-1 rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 p-2" />
                                     </label>
                                 </div>
                             </div>
@@ -131,17 +132,16 @@ const History2 = () => {
 
             </div>
 
-            {/* Phần thông tin đặt phòng */}
             <div className="lg:w-1/3 p-4 mt-20 border rounded-lg shadow-lg ml-0 lg:ml-4 bg-[#F2F0F2] h-2/3">
                 <img
                     src={roomData?.img_thumbnail}
                     alt="Large"
                     className="w-full h-60 object-cover rounded-lg shadow mb-10"
                 />
-                <strong className="text-4xl font-semibold">P.100</strong>
+                <strong className="text-4xl font-semibold">{sizeData}</strong>
                 <div className="flex items-center mt-3 mb-3">
                     <span className="flex items-center justify-center mr-2">
-                        {roomData?.price}
+                        Giá/ngày : {roomData?.price}
                     </span>
                 </div>
                 <p>{roomData?.description}</p>
@@ -150,11 +150,11 @@ const History2 = () => {
                 <div className="flex flex-col lg:flex-row space-x-0 lg:space-x-4 mb-10 mt-5">
                     <label className="text-left block w-full lg:w-1/2">
                         <strong>Ngày vào</strong>
-                        <input type="date" className="border p-1 w-full mt-1" />
+                        <input readOnly type="date" value={bookingData?.start_date} className="border p-1 w-full mt-1" />
                     </label>
                     <label className="text-left block w-full lg:w-1/2">
                         <strong>Ngày ra</strong>
-                        <input type="date" className="border p-1 w-full mt-1" />
+                        <input readOnly type="date" value={bookingData?.end_date} className="border p-1 w-full mt-1" />
                     </label>
                 </div>
 
@@ -166,11 +166,11 @@ const History2 = () => {
                                 key={item?.id}
                             >
                                 <label className="flex items-center mb-2">
-                                    <input
+                                    <input readOnly
                                         type="checkbox"
                                         className="mr-2 appearance-none bg-[#064749] border rounded-full w-4 h-4 cursor-pointer"
                                     />
-                                    <span>{item.name}</span>
+                                    <span>{item.name} ({item.price.toLocaleString("vi-VN")} VNĐ) x {item.pivot?.quantity}</span>
                                 </label>
                             </div>
                         ))
@@ -195,7 +195,7 @@ const History2 = () => {
                         <p className="text-left font-bold mt-2">Tổng:</p>
                     </div>
                     <div className="text-right">
-                        <p className="font-bold mt-2">{paymentData.payment.total_amount.toLocaleString("vi-VN")} VND</p>
+                        <p className="font-bold mt-2">{paymentData.payment.total_amount.toLocaleString("vi-VN")} VNĐ</p>
                     </div>
                 </div>
 
