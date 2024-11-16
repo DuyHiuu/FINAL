161cd0a5ebde25f2bc2a963c784b545a3c9d59bd
NgocTue <ngoctuenguyen1408@gmail.com>
2024-11-16 21:02:41 +0700

    User posts

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -69,4 +69,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Rating::class);
     }
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
 }
